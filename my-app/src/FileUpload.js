3a365f09c8c85b41db96ad2b0c9d5b6a8715c44f
bantita.b (June) <junewar.bantita@gmail.com>
2022-01-27 18:10:05 +0700

    Create pr

--- my-app/src/FileUpload.js
+++ my-app/src/FileUpload.js
@@ -3,56 +3,7 @@ import React from 'react';
 // import { useDispatch } from 'react-redux';
 
 function FileUpload({ getFilesFromUpload, selectFile, uploadFile }) {
-    // const [file, setFile] = useState('');
-    // const [data, setData] = useState([]);
-
-    // const handleChange = (event) => {
-    //     setFile(event.target.files[0]);
-    // };
-
-    // const uploadFile = (event) => {
-    //     event.preventDefault();
-
-    //     const formData = new FormData();
-    //     formData.append("file", file);
-
-    //     axios.post('http://localhost:8080/upload', formData
-    //        ).then(res => {
-    //         console.log(res);
-    //         setData({imageUrl: "http://localhost:8080/files/" + res.data.imageUrl })
-    //        }).catch(err => console.log(err))
-    // };
-
-    // console.log(data);
-
-    // อันนี้แบบ redux
-
-    // const dispatch = useDispatch();
-
-    // const addFiles = (file) => {
-    //     file.map((item) => {
-    //         dispatch(addFiles(item))
-    //     })
-    // }
-
-    // const selectFile = (event) => {
-    //     dispatch(addFiles(event.target.files[0]));
-    // };
-
-    // const uploadFile = (event) => {
-    //     event.preventDefault();
-
-    //     const formData = new FormData();
-    //     formData.append("file", getFiles);
-
-    //     axios.post('http://localhost:8080/upload', formData
-    //     ).then(res => {
-    //         console.log(res);
-    //         dispatch(getFiles({ imageUrl: "http://localhost:8080/files/" + res.data.imageUrl }))
-    //     }).catch(err => console.log(err))
-    // };
-
-    // console.log(getFiles);
+    
 
     console.log(getFilesFromUpload);
 
